34994c6d14591cd680b4d8aa9d94bbc63cd5db95
sinhle3008 <sinhle.dev@gmail.com>
2023-07-15 00:25:51 +0700

    setup cau hoi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'quiz_brain.dart';
+import 'question.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
   runApp(MyApp());
@@ -9,10 +13,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
+        body: const SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
             child: QuizPage(),
@@ -31,10 +35,101 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+
+  int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10),
+            child: Center(
+              child: Text(
+                quizBrain.questionBank[questionNumber].questionText,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20, color: Colors.white),
+                backgroundColor: Colors.green,
+              ),
+              child: const Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                //The user picked true.
+                bool correctAnswer =
+                    quizBrain.questionBank[questionNumber].questionAnswer;
+                if (correctAnswer == true) {
+                  print('Ban da chon dung dap an');
+                }else{
+                  print('Ban da chon sai dap an');
+                }
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(fontSize: 20, color: Colors.white),
+                backgroundColor: Colors.red,
+              ),
+              child:  Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                //The user picked false.
+                bool correctAnswer =
+                    quizBrain.questionBank[questionNumber].questionAnswer;
+                if (correctAnswer == false) {
+                  print('Ban da chon dung dap an');
+                }else{
+                  print('Ban da chon sai dap an');
+                }
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: [
+            Icon(
+              Icons.check,
+              color: Colors.green,
+            ),
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          ],
+        )
+      ],
     );
   }
 }
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,17 @@
+import 'question.dart';
+
+class QuizBrain{
+  List<Question> questionBank = [
+    Question(questionText: 'Cau Hoi So 1', questionAnswer: true),
+    Question(questionText: 'Cau Hoi So 2', questionAnswer: false),
+    Question(questionText: 'Cau Hoi So 3', questionAnswer: true),
+    Question(questionText: 'Cau Hoi So 4', questionAnswer: false),
+    Question(questionText: 'Cau Hoi So 5', questionAnswer: true),
+    Question(questionText: 'Cau Hoi So 6', questionAnswer: false),
+    Question(questionText: 'Cau Hoi So 7', questionAnswer: true),
+    Question(questionText: 'Cau Hoi So 8', questionAnswer: false),
+    Question(questionText: 'Cau Hoi So 9', questionAnswer: true),
+    Question(questionText: 'Cau Hoi So 10', questionAnswer: false),
+    Question(questionText: 'Cau Hoi So 11', questionAnswer: true),
+  ];
+}
